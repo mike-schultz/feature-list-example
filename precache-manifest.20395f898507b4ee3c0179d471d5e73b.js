e7e10677ff93c07809c7ecf5f580794127aa1e83
Mike Schultz <mschultz@zingsoft.com>
2019-11-22 14:20:15 -0800

    Initial commit

---
+++ precache-manifest.20395f898507b4ee3c0179d471d5e73b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fceb4257a2196b252d1f2c45b8b10b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17e91f769fc4946ea7c",
+    "url": "/static/css/main.ae4096fa.chunk.css"
+  },
+  {
+    "revision": "31b301c3ac5c6630205a",
+    "url": "/static/js/2.4b8d429b.chunk.js"
+  },
+  {
+    "revision": "f17e91f769fc4946ea7c",
+    "url": "/static/js/main.cbc734a4.chunk.js"
+  },
+  {
+    "revision": "dbad88b48f91e1828f88",
+    "url": "/static/js/runtime-main.50065ef6.js"
+  }
+]);
